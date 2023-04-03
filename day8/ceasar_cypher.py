eb8cfa73fba235abf946c49c20497c5680a78b6a
electromagneticwashingmachine <tyron.llanes@protonmail.com>
2023-04-03 20:57:31 +0400

    asar

--- day8/ceasar_cypher.py
+++ day8/ceasar_cypher.py
@@ -3,29 +3,55 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
+from ceasar_art import logo
+print(logo)
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypy(shift_amount, plain_text):
-    cipher_text = ""
-    for letter in plain_text:
-        new_position = alphabet.index(letter)
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is: {cipher_text}")
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction}d text is {end_text}")
+    
+    
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)    
+
+
+# def encrypy(shift_amount, plain_text):
+#     cipher_text = ""
+#     for letter in plain_text:
+#         position = alphabet.index(letter)
+#         new_position = position + shift_amount 
+#         cipher_text += alphabet[new_position]
+#     print(f"The encoded text is: {cipher_text}")
+    
+# def decrypt(cipher_text, shift_amount):
+#     plain_text = ""
+#     for letter in cipher_text:
+#         position = alphabet.index(letter)   
+#         new_position = position - shift_amount
+#         plain_text += alphabet[new_position]
+#     print(f"The decoded text is {plain_text}")
 
         
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+#     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+#     #e.g. 
+#     #plain_text = "hello"
+#     #shift = 5
+#     #cipher_text = "mjqqt"
+#     #print output: "The encoded text is mjqqt"
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+#     ##HINT: How do you get the index of an item in a list:
+#     #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+#     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+# #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+# if direction == "encode":
+#     encrypy(plain_text=text, shift_amount=shift)
+# elif direction == "decode":
+#     decrypt(cipher_text=text, shift_amount=shift)
 
-encrypy(plain_text=text, shift_amount=shift)
