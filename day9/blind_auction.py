d65758f06fe19e6cf0d5739c38a09efc9bba1386
electromagneticwashingmachine <tyron.llanes@protonmail.com>
2023-04-07 15:45:55 +0400

    this is the moment!

--- day9/blind_auction.py
+++ day9/blind_auction.py
@@ -4,10 +4,20 @@ from blind_art import logo
 print(logo) 
 
 bidders = {}
-bidders_name = input("What is your name?:  ")
-bid = int(input("What is your bid?: $"))
-bid_again = input("Are there any other bidder? type 'yes' or 'no'. \n")
-bidders[bidders_name] = bid
+auction = True
+while auction is True:
+    new_bidders =  {}
+    bidders_name = input("What is your name?:  ")
+    bid_value = int(input("What is your bid?: $"))
+    new_bidders[bidders_name] = bid_value
+    bidders.append(new_bidders)
+    bid_again = input("Are there any other bidder? type 'yes' or 'no'. \n")
+    if bid_again == "yes":
+        auction = True
+       
+    else:
+        auction = False
+
 
 
 print(bidders)
